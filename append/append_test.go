fcf8d9595c24924b019ebc16bc8ccf9476856609
Radu Ioan Fericean <radu@fericean.ro>
2019-06-11 18:40:55 +0100

    Improve testing

--- append/append_test.go
+++ append/append_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestAppend(t *testing.T) {
 	s := make([]int, 0, 5)
 	s = Append(s, 1)
-	if s[0] != 1 || len(s) != 1 {
+	if s[0] != 1 || len(s) != 1 || cap(s) != 5 {
 		t.Errorf("append failed: %v len=%d cap=%d", s, len(s), cap(s))
 	}
 }
@@ -13,7 +13,7 @@ func TestAppend(t *testing.T) {
 func TestAppendNil(t *testing.T) {
 	var s []int
 	s = Append(s, 1)
-	if s[0] != 1 || len(s) != 1 {
+	if s[0] != 1 || len(s) != 1 || cap(s) != 2 {
 		t.Errorf("append failed: %v len=%d cap=%d", s, len(s), cap(s))
 	}
 }
@@ -21,7 +21,7 @@ func TestAppendNil(t *testing.T) {
 func TestAppendZero(t *testing.T) {
 	s := make([]int, 0)
 	s = Append(s, 1)
-	if s[0] != 1 || len(s) != 1 {
+	if s[0] != 1 || len(s) != 1 || cap(s) != 2 {
 		t.Errorf("append failed: %v len=%d cap=%d", s, len(s), cap(s))
 	}
 }
@@ -29,7 +29,13 @@ func TestAppendZero(t *testing.T) {
 func TestAppendOverCapacity(t *testing.T) {
 	s := []int{1, 2, 3}
 	s = Append(s, 4)
-	if s[3] != 4 || len(s) != 4 {
+	if s[3] != 4 || len(s) != 4 || cap(s) != 8 {
 		t.Errorf("append failed: %v len=%d cap=%d", s, len(s), cap(s))
+	}
+	for i := range s {
+		if s[i] != i+1 {
+			t.Error("lost previous values: ", s)
+			t.FailNow()
+		}
 	}
 }
